130113e8b36b4117bd3a06aea150d9b6dec696c8
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-08-31 22:17:45 +0300

    Added forms and backends.py, so that Users may choose authorization method.

--- ProjectShop/ProjectShop/urls.py
+++ ProjectShop/ProjectShop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from UserApp.views import LoginView, RegisterView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
---
+++ ProjectShop/UserApp/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'phone_number', 'first_name', 'last_name', 'birth_date', 'password1', 'password2')
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email / Phone Number')
+
+
+class EditForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'phone_number', 'first_name', 'last_name', 'birth_date')
