b427fe80748de1f516f633684b96329e02d1f1a0
ruslan_liska <ruslan.liska@gmail.com>
2021-09-01 21:42:52 +0300

    updated models field

--- ProjectShop/order/models.py
+++ ProjectShop/order/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from UserApp.models import User
+
 
 # Create your models here.
 class Order(models.Model):
-    #order = models.IntegerField(verbose_name="ID", primary_key=True)
+    # order = models.IntegerField(verbose_name="ID", primary_key=True)
     order_date = models.DateTimeField(auto_now_add=True)
     PAYMENT_METHODS = (
         (1, "Card"),
         (2, "Cash")
     )
     payment_method = models.IntegerField(verbose_name="Payment Method", choices=PAYMENT_METHODS)
-    #user_id = models.ForeignKey("User", on_delete=models.CASCADE)
-    #shippingAddress_id = models.ForeignKey("ShippingAddress", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    # shippingAddress_id = models.ForeignKey("ShippingAddress", on_delete=models.CASCADE)
     SHIPPING_STATUSES = (
         (1, "Planning"),
         (2, "Shipping"),
         (3, "Complete")
     )
-    #shipping_status = models.IntegerField(verbose_name="Shipping status", choices=SHIPPING_STATUSES, default=1)
-
+    # shipping_status = models.IntegerField(verbose_name="Shipping status", choices=SHIPPING_STATUSES, default=1)
