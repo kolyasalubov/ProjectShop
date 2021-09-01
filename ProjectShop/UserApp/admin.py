35045abdf815c0ddcd71bd992a5dcb120c18add8
OlehHnyp <lefk6230@gmail.com>
2021-09-01 21:05:45 +0300

    Fixed self changing bug in admin site for User

--- ProjectShop/UserApp/admin.py
+++ ProjectShop/UserApp/admin.py
@@ -17,9 +17,7 @@ class UserAdmin(BaseUserAdmin):
 
     def has_change_permission(self, request, obj=None) -> bool:
         """Allow changing for superusers and for admin ot change users"""
-        if obj:
-            return not obj.role or request.user.is_superuser
-        return request.user.is_superuser
+        return request.user.role
 
     def get_form(self, request, obj=None, **kwargs):
         """Enable certain fields in form"""
@@ -29,14 +27,13 @@ class UserAdmin(BaseUserAdmin):
 
         if not is_superuser and obj != request.user and obj and not obj.role:
             enabled_fields |= {'is_active'}
-
-        if is_superuser:
-            enabled_fields |= set(form.base_fields.keys())  # going to enable all fields
-
-        if obj == request.user:
-            enabled_fields.remove('is_active')
-            if obj.role:
-                enabled_fields |= {'middle_name', 'birth_date'}
+        elif is_superuser:
+            enabled_fields |= set(form.base_fields.keys())  # going to enable all fields except 'role'
+            enabled_fields.remove('role')
+            if request.user == obj:
+                enabled_fields.remove('is_active')
+        elif obj == request.user:
+            enabled_fields |= {'middle_name', 'birth_date'}
 
         for f in form.base_fields:
             if f in enabled_fields:
@@ -47,11 +44,11 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('phone_number', 'first_name', 'last_name', 'role', 'email', 'is_superuser', 'is_active')
-    list_filter = ('is_superuser',)
+    list_display = ('phone_number', 'first_name', 'last_name', 'role', 'email', 'is_active', 'register_date', 'is_superuser')
+    list_filter = ('is_superuser', 'is_active', 'role')
     fieldsets = (
         (None, {'fields': ('phone_number', )}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name', 'birth_date', 'email')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name', 'birth_date', 'email', 'role')}),
         ('Permissions', {'fields': ('is_active',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
