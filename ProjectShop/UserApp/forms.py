a25673b0688c3aabe0ff65e6e14eabc461bc18e3
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-02 16:43:47 +0300

    birth_date field has been updated.

--- ProjectShop/UserApp/forms.py
+++ ProjectShop/UserApp/forms.py
@@ -3,8 +3,13 @@ from django.contrib.auth import get_user_model
 from django import forms
 from django.utils.translation import ugettext as _
 
+from datetime import datetime
+
 
 class RegisterForm(UserCreationForm):
+    birth_date = forms.DateField(required=False,
+                                 widget=forms.SelectDateWidget(years=range(datetime.now().year, 1900, -1)))
+
     class Meta:
         model = get_user_model()
         fields = ('email', 'phone_number', 'first_name', 'last_name', 'birth_date', 'password1', 'password2')
