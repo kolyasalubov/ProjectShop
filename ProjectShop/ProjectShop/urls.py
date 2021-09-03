a1783ab546f2863435c849138264697d74d081c1
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-03 10:42:07 +0300

    UserApp path has been changed.

--- ProjectShop/ProjectShop/urls.py
+++ ProjectShop/ProjectShop/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
-from UserApp.views import LoginView, RegisterView, AboutView
+from UserApp.views import AboutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='UserApp/logout.html'), name='logout'),
+    path('user/', include('UserApp.urls')),
     path('', AboutView.as_view(), name='home')
 ]
