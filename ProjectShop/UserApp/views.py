88938647c3d4f85fff628d5040ceae3ce20a7dfb
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-05 20:59:51 +0300

    A message has been added after a successful registration.

--- ProjectShop/UserApp/views.py
+++ ProjectShop/UserApp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import views as auth_views
 from django.views import generic
 from django.views.generic.base import TemplateView
@@ -11,10 +12,11 @@ class LoginView(auth_views.LoginView):
     template_name = 'UserApp/login.html'
 
 
-class RegisterView(generic.CreateView):
+class RegisterView(SuccessMessageMixin, generic.CreateView):
     form_class = RegisterForm
     template_name = 'UserApp/register.html'
     success_url = reverse_lazy('login')
+    success_message = "Congratulations! Your account has been created. You may sign in!"
 
 
 class AboutView(TemplateView):
