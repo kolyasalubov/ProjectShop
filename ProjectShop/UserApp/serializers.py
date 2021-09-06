cd1f7a074bde7adf3f4ef9ec7bc55fdc0b761c50
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-09-06 21:59:33 +0300

    Edited UserRestListView class and deleted UserListSerializer class

--- ProjectShop/UserApp/serializers.py
+++ ProjectShop/UserApp/serializers.py
@@ -9,9 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'first_name', 'last_name', 'birth_date', 'register_date', 'phone_number',
                   'email', 'role', 'is_active', ]
 
-
-class UserListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'first_name', 'last_name', 'phone_number', 'role', ]
-
--- ProjectShop/UserApp/views.py
+++ ProjectShop/UserApp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from UserApp.serializers import UserSerializer, UserListSerializer
+from UserApp.serializers import UserSerializer
 from UserApp.models import User
 
 
-class UserRestView(generics.RetrieveAPIView):
+class UserRestView(generics.RetrieveAPIView, generics.ListAPIView):
     """View of one user model object"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -18,6 +18,5 @@ class UserRestListView(generics.ListAPIView):
     """View list of all user model objects"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    lookup_url_kwarg = 'user_id'
     permission_classes = {IsAuthenticated, IsAdminUser}
 
