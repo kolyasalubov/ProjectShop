d8087ae5270cfffd0f634d1bc5cc6dfbd92a251a
ProkhvatylovArtem <prokhvatylov.artem@gmail.com>
2021-09-09 10:19:01 +0300

    Update product_id field, make many-to-many relation to Product.

--- ProjectShop/WishList/models.py
+++ ProjectShop/WishList/models.py
@@ -5,4 +5,4 @@ from UserApp.models import User
 
 class WishList(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    # product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # product_id = models.ManyToManyField(Product)
