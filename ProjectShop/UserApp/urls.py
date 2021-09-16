03ff541dd770dad5475fd5e3c56001b3f98cc45b
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-09-16 12:03:43 +0300

    Deleted all views

--- ProjectShop/UserApp/urls.py
+++ ProjectShop/UserApp/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from UserApp.views import UserRestView, UserRestListView
 
 urlpatterns = [
-    path('rest/<int:user_id>/', UserRestView.as_view()),
-    path('rest/all', UserRestListView.as_view())
+
 ]
 
--- ProjectShop/UserApp/views.py
+++ ProjectShop/UserApp/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from UserApp.serializers import UserSerializer
 from UserApp.models import User
-
-
-class UserRestView(generics.RetrieveAPIView, generics.ListAPIView):
-    """View of one user model object"""
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_url_kwarg = 'user_id'
-    permission_classes = (IsAuthenticated, IsAdminUser)
-
-
-class UserRestListView(generics.ListAPIView):
-    """View list of all user model objects"""
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated, IsAdminUser)
 
