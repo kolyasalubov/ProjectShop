50501ff36b7fc06bab7d3b8526dd684d70aab551
ProkhvatylovArtem <prokhvatylov.artem@gmail.com>
2021-09-16 11:16:07 +0300

    Added docstrings. Field "order_id" and "product_id" moved up and added newline after it.

--- ProjectShop/OrderItems/models.py
+++ ProjectShop/OrderItems/models.py
@@ -4,8 +4,16 @@ from django.utils.translation import gettext as _
 from order.models import Order
 
 
-# Create your models here.
 class OrderItems(models.Model):
+    """
+        A database objects that represents items in order.
+        Attributes:
+            order_id: reference to the order.
+            product_id: reference to the product.
+            quantity: amount of products in order.
+        """
+
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     # product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
     quantity = models.PositiveIntegerField(verbose_name=_("quantity"))
