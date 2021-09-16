ff1a3337be056c075ec23499a78ecf715fd7e7c4
ProkhvatylovArtem <prokhvatylov.artem@gmail.com>
2021-09-16 11:59:16 +0300

    Delete "_id" from order and products.

--- ProjectShop/OrderItems/models.py
+++ ProjectShop/OrderItems/models.py
@@ -8,12 +8,12 @@ class OrderItems(models.Model):
     """
         A database objects that represents items in order.
         Attributes:
-            order_id: reference to the order.
-            product_id: reference to the product.
+            order: reference to the order.
+            product: reference to the product.
             quantity: amount of products in order.
         """
 
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    # product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     quantity = models.PositiveIntegerField(verbose_name=_("quantity"))
