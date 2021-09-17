7179f66d8acb5bfe5d9cc03fecb6073b9fd16104
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-17 16:11:35 +0300

    Fix: a useless import line has been removed.

--- ProjectShop/UserApp/urls.py
+++ ProjectShop/UserApp/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from UserApp.views import LoginView, RegisterView, LogoutView, PasswordResetView, PasswordResetDoneView,\
