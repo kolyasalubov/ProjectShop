11239cac3ab749b326d0e4032439dc7284f00dd5
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-09-19 19:00:28 +0300

    Deleted wrong method in UserApp

--- ProjectShop/UserApp/models.py
+++ ProjectShop/UserApp/models.py
@@ -77,9 +77,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self) -> str:
         return self.email
 
-    def get_info(self):
-        return self.profile_pic.path
-
     def has_perm(self, perm, obj=None):
         return self.role
 
