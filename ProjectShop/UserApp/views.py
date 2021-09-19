98acdd629dbfd918dfdb36b2f22d6f2068b2bc85
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-19 20:41:26 +0300

    django-braces and AnonymousRequiredMixin have been added.

--- ProjectShop/UserApp/views.py
+++ ProjectShop/UserApp/views.py
@@ -6,20 +6,23 @@ from django.views import generic
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
+from django.utils.translation import ugettext_lazy as _
+
+from braces.views import AnonymousRequiredMixin
 
 from UserApp.forms import LoginForm, RegisterForm, EditForm
 
 
-class LoginView(auth_views.LoginView):
+class LoginView(AnonymousRequiredMixin, auth_views.LoginView):
     form_class = LoginForm
     template_name = 'UserApp/login.html'
 
 
-class RegisterView(SuccessMessageMixin, generic.CreateView):
+class RegisterView(AnonymousRequiredMixin, SuccessMessageMixin, generic.CreateView):
     form_class = RegisterForm
     template_name = 'UserApp/register.html'
     success_url = reverse_lazy('login')
-    success_message = "Congratulations! Your account has been created. You may sign in!"
+    success_message = _("Congratulations! Your account has been created. You may sign in!")
 
 
 class TemporalHomePageView(TemplateView):
