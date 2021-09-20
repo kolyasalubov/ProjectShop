0a75ce61c3ec015abfa82f9b2973abc660b16c40
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-09-20 18:54:10 +0300

    UpdateProfileView has been written using classes.

--- ProjectShop/UserApp/urls.py
+++ ProjectShop/UserApp/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from UserApp.views import LoginView, RegisterView, profile
+from UserApp.views import LoginView, RegisterView, UpdateProfileView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UpdateProfileView.as_view(), name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='UserApp/logout.html'), name='logout'),
-    path('profile/', profile, name='profile'),
 ]
--- ProjectShop/UserApp/views.py
+++ ProjectShop/UserApp/views.py
@@ -1,11 +1,9 @@
-from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views, get_user_model
 from django.views import generic
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
 from django.utils.translation import ugettext_lazy as _
 
 from braces.views import AnonymousRequiredMixin
@@ -29,19 +27,22 @@ class TemporalHomePageView(TemplateView):
     template_name = 'UserApp/home.html'
 
 
-@login_required
-def profile(request):
-    """
-    User must be logged in to view the profile page. Otherwise, the user is redirected to the log in page.
-    If POST request is received then validate and update the form data.
-    """
-    if request.method == 'POST':
-        form = EditForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Your account has been successfully updated!')
-            return redirect('profile')
-    else:
-        form = EditForm(instance=request.user)
+class CustomUpdateView(generic.edit.SingleObjectTemplateResponseMixin,
+                       generic.edit.ModelFormMixin,
+                       generic.edit.ProcessFormView):
 
-    return render(request, 'UserApp/profile.html', {'form': form})
+    def get(self, request, *args, **kwargs):
+        self.object = request.user
+        return super().get(request, *args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = request.user
+        return super().post(request, *args, kwargs)
+
+
+class UpdateProfileView(LoginRequiredMixin, SuccessMessageMixin, CustomUpdateView):
+    form_class = EditForm
+    model = get_user_model
+    template_name = 'UserApp/profile.html'
+    success_url = reverse_lazy('profile')
+    success_message = _("Your account has been updated!")
