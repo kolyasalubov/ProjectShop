805c180aba63ecfba05d9c700f72b150d0dab8d6
Volodymyr Lavrushko <Landadang@Gmail.com>
2021-09-20 14:00:40 +0300

    delete old models

--- TelegramBot/models.py
+++
@@ -1,9 +0,0 @@
-from client import bot_client
-
-
-class Model:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def from_json(self, json):
-        self.__init__(**json)
