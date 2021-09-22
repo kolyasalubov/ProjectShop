b84dceb52133ec37e2888281ca65728f88cc4419
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-09-22 17:14:45 +0300

    Added admin functionality with order and shipping models

--- ProjectShop/Shipping/admin.py
+++ ProjectShop/Shipping/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from Shipping.models import ShippingModel
 
-admin.site.register(ShippingModel)
+
+class ShippingModelAdmin(admin.ModelAdmin):
+    fields = ('user', 'postal_code', 'country', 'region', 'city', 'post_office')
+    list_display = ('user', 'country', 'city', 'post_office')
+    list_editable = ('post_office',)
+    search_fields = ('country', )  # doesnt work ????
+    ordering = ('country', 'region',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('user',)
+        return self.readonly_fields
+
+
+admin.site.register(ShippingModel, ShippingModelAdmin)
--- ProjectShop/order/admin.py
+++ ProjectShop/order/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 
-from order.models import OrderModel
+from order.models import OrderModel, OrderItemsModel
 
 
-admin.site.register(OrderModel)
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItemsModel
+    readonly_fields = ('order_items_qantity',)
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ('user', 'payment_method', 'shipping_status', 'payment_status', 'shippingAddress_id', 'order_date')
+    readonly_fields = ('order_date', )
+    inlines = (OrderItemsInline, )
+    list_display = ('user', 'payment_status', 'shipping_status', )
+    list_editable = ('payment_status', 'shipping_status', )
+    list_filter = ('payment_status', 'shipping_status', )
+    ordering = ('payment_status', 'shipping_status', )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('user', 'payment_method', 'shippingAddress_id')
+        return self.readonly_fields
+
+
+admin.site.register(OrderModel, OrderAdmin)
+admin.site.register(OrderItemsModel)
 
