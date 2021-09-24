6184ecac262c53f7e752c5feae83f86dcc0e71ce
Andriisas <andriisas16@gmail.com>
2021-09-24 21:58:19 +0300

    commit

---
+++ ProjectShop/UserApp/tests/factories.py
@@ -0,0 +1,22 @@
+import factory
+from UserApp.models import User
+from faker import Faker
+import phonenumbers
+
+
+def fake_phone_number() -> str:
+    fake = Faker('uk_UA')
+    return fake.phone_number()
+     
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    birth_date = factory.Faker('date_of_birth')
+    phone_number = factory.LazyFunction(fake_phone_number)
+    email = factory.Faker('email')
+    password = factory.PostGenerationMethodCall('set_password', 'ValidPassword1@')
+    
---
+++ ProjectShop/UserApp/tests/test_forms.py
@@ -0,0 +1,110 @@
+from django.test import TestCase
+from UserApp.forms import RegisterForm, EditForm, LoginForm
+from UserApp.tests.factories import UserFactory
+from django.utils.crypto import get_random_string
+
+
+class TestForms(TestCase):
+    def test_RegisterForm_valid_data(self):
+        user = UserFactory.build()
+        form = RegisterForm(data={
+            'email' : user.email,
+            'first_name' : user.first_name,
+            'last_name' : user.last_name,
+            'phone_number' : user.phone_number,
+            'birth_date' : user.birth_date,
+            'password1' : 'ValidPassword1@',
+            'password2' : 'ValidPassword1@'
+        })
+
+        self.assertFalse(form.is_valid())
+        # print(form.errors)
+        # print(user.phone_number)
+        # print(type(user.phone_number))
+        # print(user.email)
+        print(user.password)
+
+
+    # def test_RegisterForm_invalid_data(self):
+    #     form = RegisterForm(data={
+    #         'email' : '',
+    #         'first_name' : '',
+    #         'last_name' : '',
+    #         'phone_number' : '',
+    #         'birth_date' : '',
+    #         'password1' : '',
+    #         'password2' : ''
+    #     })
+
+    #     self.assertFalse(form.is_valid())
+
+    # def test_RegisterForm_no_data(self):
+    #     form = RegisterForm(data={})
+
+    #     self.assertFalse(form.is_valid())
+
+    # def test_EditForm_valid(self):
+    #     user = UserFactory.build()
+    #     form = EditForm(data={
+    #         'phone_number' : user.phone_number,
+    #         'first_name' : user.first_name,
+    #         'last_name' : user.last_name,
+    #         'birth_date' : user.birth_date
+
+    #     })
+    #     self.assertTrue(form.is_valid())
+    #     # print(user.phone_number)
+    #     # print(user.email)
+    #     # print(form.errors)
+
+    # def test_EditForm_invalid_data(self):
+    #     form = EditForm(data={
+    #         'phone_number' : '',
+    #         'first_name' : '',
+    #         'last_name' : '',
+    #         'birth_date' : ''
+    #     })
+
+    #     self.assertFalse(form.is_valid())
+
+    # def test_EditForm_no_data(self):
+    #     form = EditForm(data={})
+
+    #     self.assertFalse(form.is_valid())
+
+    # def test_LoginForm_valid_email(self):
+    #     user = UserFactory()
+    #     form = LoginForm(data={
+    #         'username' : user.email,
+    #         'password' : '1234'
+    #     })
+
+    #     self.assertTrue(form.is_valid())
+    #     print(form.errors)
+    #     print(user.email)
+    #     print(user.password)
+
+    # def test_LoginForm_valid_phone(self):
+    #     user = UserFactory()
+    #     form = LoginForm(data={
+    #         'username' : user.phone_number,
+    #         'password' : '1234'
+    #     })
+
+    #     self.assertTrue(form.is_valid())
+    #     print(form.errors)
+    #     print(user.phone_number)
+    #     print(user.password)
+    
+    # def test_LoginForm_invalid_data(self):
+    #     user = UserFactory()
+    #     form = LoginForm(data={
+    #         'username' : '',
+    #         'password' : ''
+    #     })
+    #     self.assertFalse(form.is_valid())
+
+    # def test_LoginForm_no_data(self):
+    #     user = UserFactory()
+    #     form = LoginForm(data={})
+    #     self.assertFalse(form.is_valid())
