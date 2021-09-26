05498fe5caca5fc50448f77faf8ae35fb920aef4
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-09-26 17:20:14 +0300

    Added dropdown filter to shipping model

--- ProjectShop/Shipping/admin.py
+++ ProjectShop/Shipping/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from Shipping.models import ShippingModel
 
+from django.contrib.admin.filters import ChoicesFieldListFilter
+
+
+class DropdownFilter(ChoicesFieldListFilter):
+    template = 'admin/dropdown_filter.html'
+
 
 class ShippingModelAdmin(admin.ModelAdmin):
     fields = ('user', 'postal_code', 'country', 'region', 'city', 'post_office')
     list_display = ('user', 'country', 'city', 'post_office')
     list_editable = ('post_office',)
+    list_filter = (('country', DropdownFilter), )
     ordering = ('country', 'region',)
 
     def get_readonly_fields(self, request, obj=None):
