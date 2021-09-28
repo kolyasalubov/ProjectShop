74d88fb012a4396106adfea0019803e476d2fc32
ruslanliska <ruslan.liska@gmail.com>
2021-09-28 11:44:36 +0300

    Modified due to PEP* rules

--- ProjectShop/order/views.py
+++ ProjectShop/order/views.py
@@ -2,9 +2,7 @@ from rest_framework import viewsets, renderers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from order.models import Order
-from order.models import OrderItems
-
+from order.models import Order, OrderItems
 from order.serializers import OrderDetailSerializer, OrderItemsSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
