ed29146dff3ea8f9fdccdd7fcd38b2ad1750046f
Max Pulyk <43215127+Maxgioman@users.noreply.github.com>
2021-09-28 11:31:38 +0300

    Changed ModelViewSet to ReadOnlyModelViewSet
    changed to read-only to prevent users from editing products

--- ProjectShop/ProductApp/views.py
+++ ProjectShop/ProductApp/views.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
@@ -8,31 +8,31 @@ from ProductApp.serializers import (ProductSerializer, ProductCategorySerializer
                                     ReviewSerializer, TagSerializer)
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(ReadOnlyModelViewSet):
     """This is viewset for Product model"""
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
-class ProductCategoryViewSet(ModelViewSet):
+class ProductCategoryViewSet(ReadOnlyModelViewSet):
     """This is viewset for ProductCategory model"""
     serializer_class = ProductCategorySerializer
     queryset = ProductCategory.objects.all()
 
 
-class ProductSubcategoryViewSet(ModelViewSet):
+class ProductSubcategoryViewSet(ReadOnlyModelViewSet):
     """This is viewset for ProductSubcategory model"""
     serializer_class = ProductSubcategorySerializer
     queryset = ProductSubcategory.objects.all()
 
 
-class TagViewSet(ModelViewSet):
+class TagViewSet(ReadOnlyModelViewSet):
     """This is viewset for Tag model"""
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
 
 
-class ProductMediaViewSet(ModelViewSet):
+class ProductMediaViewSet(ReadOnlyModelViewSet):
     """This is viewset for ProductMedia model"""
     serializer_class = ProductMediaSerializer
     queryset = ProductMedia.objects.all()
