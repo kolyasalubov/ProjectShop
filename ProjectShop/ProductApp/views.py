9a5abec45e65e8d551bb6c30a4df0af834c94b6f
Volodymyr Lavrushko <Landadang@Gmail.com>
2021-09-28 12:32:07 +0300

    common router

--- ProjectShop/ProductApp/views.py
+++ ProjectShop/ProductApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from rest_framework.viewsets import ModelViewSet
 
 from ProductApp.models import Review, Product
--- ProjectShop/ProjectShop/router.py
+++ ProjectShop/ProjectShop/router.py
@@ -1,5 +1,8 @@
 from rest_framework import routers
+
+from ProductApp.views import ReviewViewSet
 from order.views import OrderViewSet
 
 router = routers.DefaultRouter()
 router.register('order', OrderViewSet, basename='order')
+router.register(r'products/(?P<product_id>\d+)/reviews', ReviewViewSet, basename="reviews")
