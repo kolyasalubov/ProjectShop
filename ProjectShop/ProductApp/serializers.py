99be267d5ea1a3a88585da9f8eca5e977123f64e
Maxgioman <maximchuk2001@gmail.com>
2021-10-05 21:54:31 +0300

    ProductApp serializers hotfix
    reworked serializers for ProductApp models

--- ProjectShop/ProductApp/serializers.py
+++ ProjectShop/ProductApp/serializers.py
@@ -1,28 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from ProductApp.models import Product, ProductMedia, ProductCategory, ProductSubcategory, Tag, Review
-from UserApp.serializers import UserSerializer
 
 
 class ProductCategorySerializer(ModelSerializer):
 	"""Serializer for ProductCategory model"""
 	class Meta:
 		model = ProductCategory
-		fields = '__all__'
+		fields = ['name']
 
 
 class ProductSubcategorySerializer(ModelSerializer):
 	"""Serializer for ProductSubcategory model"""
 	class Meta:
 		model = ProductSubcategory
-		fields = '__all__'
+		fields = ['name']
 
 
 class TagSerializer(ModelSerializer):
 	"""Serializer for Tag model"""
 	class Meta:
 		model = Tag
-		fields = '__all__'
+		fields = ['name']
 
 
 class ProductSerializer(ModelSerializer):
@@ -30,15 +29,24 @@ class ProductSerializer(ModelSerializer):
 	categories = ProductCategorySerializer(many=True, read_only=True)
 	subcategories = ProductSubcategorySerializer(many=True, read_only=True)
 	tags = TagSerializer(many=True, read_only=True)
+	media = SlugRelatedField(read_only=True, slug_field='video_link')
 
 	class Meta:
 		model = Product
-		fields = '__all__'
+		fields = ['id', 'name', 'price', 'description', 'stock_quantity',
+				  'categories', 'subcategories', 'tags', 'media']
+
+
+class ProductNameSerializer(ModelSerializer):
+	"""Serializer for Product model with only name"""
+	class Meta:
+		model = Product
+		fields = ['name']
 
 
 class ProductMediaSerializer(ModelSerializer):
 	"""Serializer for ProductMedia model"""
-	product = ProductSerializer(read_only=True)
+	product = ProductNameSerializer(read_only=True)
 
 	class Meta:
 		model = ProductMedia
@@ -46,9 +54,9 @@ class ProductMediaSerializer(ModelSerializer):
 
 
 class ReviewSerializer(ModelSerializer):
-    """
-    Serializer for Review view. Replies for review (likes and dislikes) will be added later
-    """
-    class Meta:
-        model = Review
-        fields = "__all__"
+	"""Serializer for Review view. Replies for review (likes and dislikes) will be added later"""
+	product = ProductNameSerializer(read_only=True)
+
+	class Meta:
+		model = Review
+		fields = "__all__"
