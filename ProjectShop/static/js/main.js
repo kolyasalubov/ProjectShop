82f0240e4882639c13c5afdef844bf850132b3d4
l3va <ggwpleva@gmail.com>
2021-10-09 17:33:48 +0300

    implement header and footer(except search button) in UserApp/_base.html and downloaded icons in static/assets)

--- ProjectShop/static/js/main.js
+++ ProjectShop/static/js/main.js
@@ -1,3 +1,4 @@
+// burger menu
 const menuBtn = document.querySelector('.hamburger');
 const navBar = document.querySelector('.burger-nav');
 const body = document.querySelector('body');
@@ -13,3 +14,21 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+// making header fixed
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
