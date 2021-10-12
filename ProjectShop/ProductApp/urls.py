2bc78714a64ebff9e1990dd661a1dc96e3541135
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-10-12 21:45:50 +0300

    Search functionality has beed added.

--- ProjectShop/ProductApp/urls.py
+++ ProjectShop/ProductApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ProductApp.views import HomePageView, CategoriesView
+from ProductApp.views import HomePageView, CategoriesView, SearchResultsListView
 
 
 urlpatterns = [
     path('categories/', CategoriesView.as_view(), name='categories'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- ProjectShop/ProductApp/views.py
+++ ProjectShop/ProductApp/views.py
@@ -5,11 +5,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from ProductApp.models import Review, Product, ProductCategory
 from ProductApp.serializers import ReviewSerializer
+from ProductApp.forms import SearchForm
 
 
-class HomePageView(generic.ListView):
+class HomePageView(generic.ListView, generic.FormView):
     context_object_name = 'products'
     template_name = 'ProductApp/homepage.html'
+    form_class = SearchForm
     queryset = Product.objects.all()
     paginate_by = 12
 
@@ -25,6 +27,23 @@ class CategoriesView(generic.ListView):
     context_object_name = 'categories'
     template_name = 'ProductApp/categories.html'
     paginate_by = 12
+
+
+class SearchResultsListView(generic.ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'ProductApp/search_results.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Product.objects.filter(name__icontains=query, stock_quantity__gt=0)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = ProductCategory.objects.order_by('name')[:20]
+        context['categories'] = categories
+        return context
 
 
 class ReviewViewSet(ModelViewSet):
