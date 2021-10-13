aa2ab35d6652d5e16e38b036b48c4805b2267792
Volodymyr Lavrushko <Landadang@Gmail.com>
2021-10-13 22:25:41 +0300

    structurize and add handlers for filters

--- TelegramBot/dispatcher.py
+++ TelegramBot/dispatcher.py
@@ -1,11 +1,12 @@
 import telegram
 from telegram.ext import (
-    Updater, Dispatcher, Filters,
+    Updater, Filters,
     CommandHandler, MessageHandler, CallbackQueryHandler,
 )
 
 from TOKEN import TELEGRAM_TOKEN
-from handlers.product_manager import propose_categories, turn_categories_page
+from handlers.product_manager import chosen_category, CategoryCallbacks, \
+    SubcategoryCallbacks, TagCallbacks
 from handlers.user_menu import BUTTON_SHOW_PRODUCTS, get_base_reply_keyboard
 
 
@@ -20,9 +21,19 @@ def start_command(update, context):
 def setup_dispatcher(dp):
     """ Setupping dispatcher and adding all handlers """
     dp.add_handler(CommandHandler('start', start_command))
-    dp.add_handler(MessageHandler(Filters.text('search products'), propose_categories, pass_chat_data=True))
-    dp.add_handler(CallbackQueryHandler(turn_categories_page, pass_chat_data=True,
-                                        pattern=r'^.+products/categories.+$'))
+    dp.add_handler(MessageHandler(Filters.text('search products'), CategoryCallbacks.propose_page, pass_chat_data=True))
+    dp.add_handler(CallbackQueryHandler(CategoryCallbacks.turn_page, pass_chat_data=True,
+                                        pattern=r'^http://.+/categories/.+$'))
+    dp.add_handler(CallbackQueryHandler(chosen_category, pass_chat_data=True,
+                                        pattern=r'Category=.+'))
+    dp.add_handler(CallbackQueryHandler(SubcategoryCallbacks.propose_page, pass_chat_data=True,
+                                        pattern=r"filters=Yes"))
+    dp.add_handler(CallbackQueryHandler(SubcategoryCallbacks.turn_page, pass_chat_data=True,
+                                        pattern=r'^http://.+/subcategories/.+$'))
+    dp.add_handler(CallbackQueryHandler(TagCallbacks.propose_page, pass_chat_data=True,
+                                        pattern=r'^finish-Subcategory'))
+    dp.add_handler(CallbackQueryHandler(TagCallbacks.turn_page, pass_chat_data=True,
+                                        pattern=r'^http://.+/tags/.+$'))
     return dp
 
 
@@ -34,7 +45,7 @@ def run_pooling():
     dp = setup_dispatcher(dp)
 
     bot_info = telegram.Bot(TELEGRAM_TOKEN).get_me()
-    bot_link = f"https://t.me/" + bot_info["username"]
+    bot_link = f"https://t.me/{bot_info['username']}"
 
     print(f"Pooling of '{bot_link}' started")
     updater.start_polling()
--- TelegramBot/handlers/product_manager.py
+++ TelegramBot/handlers/product_manager.py
@@ -1,22 +1,113 @@
-from client.models import Category
-from handlers.general import create_keyboard
+from time import time
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from client.models import Category, Page, Subcategory, Tag
+from handlers.utils import KeyboardBuilder
 
 
-def propose_categories(update, context):
+class PageCallbacks:
     """
-    Callback function for message handler 'search products'. Returns to bot InlineKeyboard with first page of categories
+    Base class for all models, that require a list of buttons.
+    Implementation require only a specification of return_class.
     """
-    categories = Category.get()
-    print(categories)
-    keyboard = create_keyboard(categories, 2)
-    message = context.bot.send_message(chat_id=update.message.chat_id, text="Choose category:", reply_markup=keyboard)
-    if context.chat_data['category_list']:
-        context.bot.delete_message(chat_id=update.message.chat_id, message_id=context.chat_data['category_list'])
-    context.chat_data['category_list'] = message.id
+    return_class = None
+    text = ""
+
+    @classmethod
+    def _build_keyboard(cls, page: Page) -> KeyboardBuilder:
+        print(cls.return_class.__name__)
+        keyboard_builder = KeyboardBuilder(page=page, data=f"{cls.return_class.__name__}")
+        keyboard_builder.create_keyboard()
+        return keyboard_builder
+
+    @classmethod
+    def propose_page(cls, update: Update, context: CallbackContext):
+        """
+        Callback function for creating keyboard lists. Returns to bot InlineKeyboard with first page of values
+        """
+        page = cls.return_class.get()
+        last_message = update.message or update.callback_query.message
+        message = context.bot.send_message(
+            chat_id=last_message.chat_id,
+            text=cls.text,
+            reply_markup=cls._build_keyboard(page).keyboard
+        )
+        context.bot.delete_message(chat_id=last_message.chat_id, message_id=last_message.message_id)
+
+        #       if user has already a list of specific vales, it is deleted from chat
+        if f"{cls.return_class.__name__}_list" in context.chat_data:
+            context.bot.delete_message(
+                chat_id=update.message.chat_id,
+                message_id=context.chat_data[f"{cls.return_class.__name__}_list"],
+            )
+
+        #       after that, we are saving id of message in bot's memory
+        context.chat_data[f"{cls.return_class.__name__}_list"] = message.message_id
+
+    @classmethod
+    def turn_page(cls, update: Update, context: CallbackContext):
+        """
+        Callback function for CallbackQueryHandler with path for turning pages.
+        """
+        page = cls.return_class.turn_page(url=update.callback_query.data)
+        context.bot.edit_message_reply_markup(
+            chat_id=update.callback_query.message.chat_id,
+            message_id=update.callback_query.message.message_id,
+            reply_markup=cls._build_keyboard(page).keyboard,
+        )
 
 
-def turn_categories_page(update, context):
+class CategoryCallbacks(PageCallbacks):
+    return_class = Category
+    text = "Choose category:"
+
+
+class SubcategoryCallbacks(PageCallbacks):
+    return_class = Subcategory
+    text = "Choose subcategories for filter:"
+
+    @classmethod
+    def _build_keyboard(cls, page: Page) -> KeyboardBuilder:
+        keyboard_builder = super()._build_keyboard(page)
+        keyboard_builder.add_finish_button()
+        return keyboard_builder
+
+
+class TagCallbacks(PageCallbacks):
+    return_class = Tag
+    text = "Choose tags for filter:"
+
+    @classmethod
+    def _build_keyboard(cls, page: Page) -> KeyboardBuilder:
+        keyboard_builder = super()._build_keyboard(page)
+        keyboard_builder.add_finish_button()
+        return keyboard_builder
+
+
+def chosen_category(update: Update, context: CallbackContext):
     """
-    Callback function for CallbackQueryHandler with path for turning pages.
+    Callback function for CallbackQueryHandler. After user chose category, he is asked if he wants to apply filters
     """
-    print(update)
+    context.bot.delete_message(
+        chat_id=update.callback_query.message.chat_id,
+        message_id=context.chat_data["Category_list"],
+    )
+    del context.chat_data["Category_list"]
+    context.chat_data["chosen_category"] = update.callback_query.data.replace(
+        "category=", ""
+    )
+    keyboard_builder = KeyboardBuilder(Page(results=["Yes", "No"]), "filters")
+    keyboard_builder.create_keyboard(columns=2)
+    context.bot.send_message(
+        chat_id=update.callback_query.message.chat_id,
+        text="Do you want to apply any filters?",
+        reply_markup=keyboard_builder.keyboard
+    )
+
+
+def filters_subcategories(update: Update, context: CallbackContext):
+    """
+    Callback function for applying filters. One for subcategories
+    """
