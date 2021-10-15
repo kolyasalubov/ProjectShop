181ac87f1ffbce16c220217209ee94c1492e67cc
OlehHnyp <lefk6230@gmail.com>
2021-10-15 10:38:07 +0300

    Added additional compose-file for only db running

--- ProjectShop/ProjectShop/settings.py
+++ ProjectShop/ProjectShop/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
