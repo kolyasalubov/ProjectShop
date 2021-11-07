776276af9a8f6862a33c28503e91ec1d897ab810
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-11-07 18:55:28 +0200

    Small PEP8 changes

--- ProjectShop/ProductApp/tests/test_product_detail.py
+++ ProjectShop/ProductApp/tests/test_product_detail.py
@@ -10,7 +10,7 @@ class ProductDetailTest(TestCase):
 
     def setUp(self):
         self.product = ProductFactory()
-        url = reverse('product-detail', args=(self.product.slug,))
+        url = reverse("product-detail", args=(self.product.slug,))
         self.response = self.client.get(url)
 
     def test_product_detail_status_code(self):
@@ -18,19 +18,19 @@ class ProductDetailTest(TestCase):
 
     def test_product_detail_template(self):
         self.assertTemplateUsed(self.response,
-                                'ProductApp/product_detail.html'
+                                "ProductApp/product_detail.html"
                                 )
 
     def test_product_detail_contains_correct_html(self):
-        self.assertContains(self.response, f'{self.product.name}')
-        self.assertContains(self.response, f'{self.product.price}')
-        self.assertContains(self.response, f'{self.product.description}')
+        self.assertContains(self.response, f"{self.product.name}")
+        self.assertContains(self.response, f"{self.product.price}")
+        self.assertContains(self.response, f"{self.product.description}")
 
     def test_product_detail_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, 'I should not be on the page')
+        self.assertNotContains(self.response, "I should not be on the page")
 
     def test_product_detail_url_resolves_product_datailview(self):
-        view = resolve(f'/{self.product.slug}/')
+        view = resolve(f"/{self.product.slug}/")
         self.assertEqual(view.func.__name__,
                          ProductDetailView.as_view().__name__
                          )
@@ -43,7 +43,7 @@ class ProductDetailViewTest(TestCase):
 
     def test_attrs(self):
         self.assertEqual(self.view.model, Product)
-        self.assertEqual(self.view.context_object_name, 'product_detail')
+        self.assertEqual(self.view.context_object_name, "product_detail")
         self.assertEqual(self.view.template_name,
-                         'ProductApp/product_detail.html'
+                         "ProductApp/product_detail.html"
                          )
