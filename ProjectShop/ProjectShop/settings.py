a14d76b71c91ae3dfc6dfaca17824ab40734092b
OlehHnyp <lefk6230@gmail.com>
2021-11-15 22:22:47 +0200

    commit

--- ProjectShop/ProjectShop/settings.py
+++ ProjectShop/ProjectShop/settings.py
@@ -182,7 +182,6 @@ else:
         }
     )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CKEDITOR_UPLOAD_PATH = "ckeditor_uploads/"
