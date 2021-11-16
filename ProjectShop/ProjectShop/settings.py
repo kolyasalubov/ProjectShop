eaff86c17c276b1c9fb64b12269b605d859a7eb6
OlehHnyp <lefk6230@gmail.com>
2021-11-16 02:21:17 +0200

    kamikadze is ready for ultimate deploying

--- ProjectShop/ProjectShop/settings.py
+++ ProjectShop/ProjectShop/settings.py
@@ -156,21 +156,18 @@ if DEPLOYMENT:
     AWS_ACCESS_KEY_ID = get_environment_variable("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = get_environment_variable("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = get_environment_variable("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-3.amazonaws.com"
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     # s3 static settings
     STATIC_LOCATION = "static"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "ProjectShop.storage_backends.StaticStorage"
+    STATICFILES_STORAGE = 'ProjectShop.storage_backends.StaticStorage'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = "media"
     MEDIA_URL = f"{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
     DEFAULT_FILE_STORAGE = "ProjectShop.storage_backends.PublicMediaStorage"
 else:
-    STATIC_URL = "/static/"
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     DATABASES['default'].update(
         {
             "ENGINE": get_environment_variable("SQL_ENGINE"),
@@ -181,8 +178,12 @@ else:
             "PORT": get_environment_variable("SQL_PORT"),
         }
     )
+    STATIC_URL = "/staticfiles/"
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 CKEDITOR_UPLOAD_PATH = "ckeditor_uploads/"
 # Default primary key field type
